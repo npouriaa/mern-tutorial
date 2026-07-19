2efc3d6ea91040723e1dc568421be926cf2b3c87
Pouria Navipour <npouriaa@gmail.com>
2026-07-19 11:50:23 +0330

    chore: initialize project with package.json and essential dependencies

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,42 @@
+const Joi = require("joi");
+
+// @desc    Get all goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = async (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc    Create a goal
+// @route   POST /api/goals
+// @access  Private
+const setGoal = async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(1000).required(),
+  });
+
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).send({
+      error: validationResult.error.details[0].message,
+    });
+  }
+
+  res.status(201).json({ message: "Set goal" });
+};
+
+// @desc    Update a goal
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = async (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// @desc    Delete a goal
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = async (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json())
+
+
+app.use("/api/goals" , require("./routes/goalRoutes"))
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
