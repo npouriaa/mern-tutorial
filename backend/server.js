8a8af4fe27745288a821508a505ee050fa4bd7c7
Pouria Navipour <npouriaa@gmail.com>
2026-07-21 13:05:22 +0330

    feat: set up MongoDB connection and add goal model

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
+connectDB();
 
-app.use("/api/goals" , require("./routes/goalRoutes"))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
